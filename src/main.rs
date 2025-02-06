c816cd152619619dc82cf1da79811df5c92b0298
Aiden Turner <yosefaiden@gmail.com>
2025-02-06 18:50:22 -0500

    started work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
